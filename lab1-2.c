4d306b74b30bc6d4a2453f6dd178ce708f4bc9d9
6704062611221 <s6704062611221@email.kmutnb.ac.th>
2025-12-09 15:29:34 +0700

    Add files via upload

--- lab1-2.c
+++ lab1-2.c
@@ -4,13 +4,19 @@
       scanf("%d",&a);
       float sum = (a * 0.05);
             sum = a - sum ;
-      printf("%.2f\n",sum);
+      float vat = sum * 0.07;
+            sum = sum + vat;
+      printf("Promotion + Vat = %.2f\n",sum);
       scanf("%d",&b);
       float sumb = (a+b) * 0.15;
        sumb = (a+b) - sumb;
-      printf("%.2f\n",sumb);
+       float vat2 = sumb * 0.07;
+       sumb = sumb + vat2;
+      printf("Promotion + vat =  %.2f\n",sumb);
       scanf("%d",&c);
       float sumc = (a+b+c) * 0.30;
        sumc = (a+b+c) - sumc;
-      printf("%.2f\n",sumc);
+      float vat3 = sumc * 0.07;
+       sumc = sumc + vat3;
+      printf("Promotion + vat = %.2f\n",sumc);
   }
